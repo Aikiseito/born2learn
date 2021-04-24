c59d9c11f16429d364e78fed18cc0993aa57f3ef
Aikiseito <79541597+Aikiseito@users.noreply.github.com>
2021-04-24 11:52:53 +0300

    Update pdp11.h

--- pdp11.h
+++ pdp11.h
@@ -7,13 +7,40 @@ typedef unsigned char byte; 	// 8 bit
 typedef unsigned short int word; // 16 bit
 typedef word adr; 				// 16 bit
 
+typedef struct {
+	word val;
+	word adr;
+} Arg;
+
+typedef struct {
+	Arg ss;
+	Arg dd;
+} Par;
+
+typedef struct {
+	word mask;
+	word opcode;
+	char * name;
+	char param;
+	void (* do_func)(Par p);
+} Command;
+
 extern byte mem[MEMSIZE]; 		//Память
 extern word reg[8]; 			//Регистры R0, ..., R8
+extern Arg ss, dd;
+extern Command cmd[];
 
 byte b_read  (adr a);
 void b_write (adr a, byte val);
 word w_read  (adr a);
 void w_write (adr a, word val);
+void run();
+Arg get_mr(word w);
+
+void do_halt();
+void do_mov();
+void do_add();
+void do_nothing();
 
 void trace(const char * format, ...);
 void load_file (const char * filename);
